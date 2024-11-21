37aba274d6f67ca8ea074edb5cf138320c8ca3a9
Ainhoa <1749587@uab.cat>
2024-11-21 18:04:53 +0100

    Opcio-Menu C2

--- Premis.sh
+++ Premis.sh
@@ -15,57 +15,48 @@ menu() {
 }
 
 enDesenvolupament() {
-	echo "---------------------------------------------------------"
-	echo "Catàleg de guanyadors i guanyadores d’Oscars"
-	echo "--------------------------------------------------------- "
-	echo "1 Llistats del catàleg."
-	echo "2 Cerca al catàleg."
-	echo "3 Com ets d'Expert?"
-	echo "4 Gestió del catàleg."
-	echo "0 Sortir."
-	echo "Introdueix una opció: "
-	read opcio
+	clear
+	echo "En desenvolupament"
 }
 
-
-if [ $opcio -eq 0 ];
-then
-	exit 0
-elif [ $opcio -eq 1 ];
-then
-	clear
-	echo "En desenvolupament"
-	read tecla
-	clear
-	menu
-	
-elif [ $opcio -eq 2 ];
-then
-	clear
-	echo "En desenvolupament"
-	read tecla
-	clear
-	menu
-	
-elif [ $opcio -eq 3 ];
-then
-	clear
-	echo "En desenvolupament"
-	read tecla
-	clear
-	menu
-	
-elif [ $opcio -eq 4 ];
-then
-	clear
-	echo "En desenvolupament"
-	read tecla
-	clear
-	menu
-else
-	clear
+opcioNoValida() {
 	echo "Error: Opció no vàlida"
 	sleep 2
 	clear
-	menu
-fi
+}
+
+
+case $opcio in
+	0)
+		exit 0
+		;;
+	1)
+		clear
+		enDesenvolupament
+		read tecla
+		clear
+		;;
+	2)
+		clear
+		enDesenvolupament
+		read tecla
+		clear
+		;;
+	3)
+		clear
+		enDesenvolupament
+		read tecla
+		clear
+		;;
+	4)
+		clear
+		enDesenvolupament
+		read tecla
+		clear
+		;;
+	*)
+		clear
+		opcioNoValida
+		menu
+		clear
+easc
